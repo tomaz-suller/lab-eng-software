6b883fad877262a2c0cd36b48dae2b44fd38e4fa
unknown <11806642>
2022-10-07 17:13:20 -0300

    Add models Companhia Aerea and Voo

--- voos/models.py
+++ voos/models.py
@@ -1,3 +1,17 @@
+from lzma import _OpenTextWritingMode
 from django.db import models
 
 # Create your models here.
+class CompanhiaAerea(models.Model):
+    nome = models.CharField(max_length=100)
+    sigla = models.CharField(max_length=5)
+    class Meta:
+        db_table = 'companhias_aereas'
+
+class Voo(models.Model):
+    codigo = models.CharField(max_length=6, primary_key=True)
+    origem = models.CharField(max_length=100)
+    destino = models.CharField(max_length=100)
+    companhia_aerea = models.ForeignKey(CompanhiaAerea)
+    class Meta:
+        db_table = 'voos'
