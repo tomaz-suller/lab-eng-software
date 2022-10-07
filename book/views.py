c25d2c244cfb5c5bbd0492b5b1171678855f4f5e
tomaz-suller <tomaz.suller@usp.br>
2022-10-07 16:29:20 -0300

    Delete placeholder app

--- book/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def bookview(request):
-    return render(request, "FIRST.html")
--- monitoramento_avioes/urls.py
+++ monitoramento_avioes/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from book.views import bookview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('FIRST', bookview),
 ]
