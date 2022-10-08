45478f53d5d4a8b116126f5cdeff9dc3de97341e
tomaz-suller <tomaz.suller@usp.br>
2022-10-08 15:04:33 -0300

    Set deliberate on_delete on all models

--- voos/models.py
+++ voos/models.py
@@ -1,8 +1,6 @@
-# from lzma import _OpenTextWritingMode
 from django.db import models
 
 
-# Create your models here.
 class Estado(models.Model):
     nome = models.CharField(max_length=15)
 
@@ -22,7 +20,9 @@ class Voo(models.Model):
     codigo = models.CharField(max_length=6, primary_key=True)
     origem = models.CharField(max_length=100)
     destino = models.CharField(max_length=100)
-    companhia_aerea = models.ForeignKey(CompanhiaAerea, on_delete=models.CASCADE)
+    companhia_aerea = models.ForeignKey(
+        CompanhiaAerea, null=True, on_delete=models.SET_NULL
+    )
 
     class Meta:
         db_table = "voos"
@@ -30,11 +30,11 @@ class Voo(models.Model):
 
 class InstanciaVoo(models.Model):
     partida_prevista = models.DateTimeField()
-    partida_real = models.DateTimeField()
+    partida_real = models.DateTimeField(null=True)
     chegada_prevista = models.DateTimeField()
-    chegada_real = models.DateTimeField()
-    estado_atual = models.ForeignKey(Estado, on_delete=models.CASCADE)
-    voo = models.ForeignKey(Voo, on_delete=models.CASCADE)
+    chegada_real = models.DateTimeField(null=True)
+    estado_atual = models.ForeignKey(Estado, on_delete=models.PROTECT)
+    voo = models.ForeignKey(Voo, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         db_table = "instancias_voos"
@@ -42,13 +42,15 @@ class InstanciaVoo(models.Model):
 
 class Movimentacao(models.Model):
     data_movimentacao = models.DateTimeField()
-    tempo_movimentacao = models.DurationField()
+    tempo_movimentacao = models.DurationField(null=True)
     instancia_voo = models.ForeignKey(InstanciaVoo, on_delete=models.CASCADE)
+    # related_name of '+' does not allow accessing Movimentacao from Estado
+    # See https://docs.djangoproject.com/en/4.1/ref/models/fields/#django.db.models.ForeignKey.related_name  # noqa: E501
     estado_anterior = models.ForeignKey(
-        Estado, on_delete=models.CASCADE, related_name="estado_anterior"
+        Estado, on_delete=models.PROTECT, related_name="+"
     )
     estado_posterior = models.ForeignKey(
-        Estado, on_delete=models.CASCADE, related_name="estado_posterior"
+        Estado, on_delete=models.PROTECT, related_name="+"
     )
 
     class Meta:
