a62cc8a67419699b5c2ee14ec7047826a471ecbd
tomaz-suller <tomaz.suller@usp.br>
2022-10-15 11:59:55 -0300

    Add dummy index template

--- voos/views.py
+++ voos/views.py
@@ -1,9 +1,16 @@
+from django.shortcuts import render
 from django.views.generic import ListView
 
 from .models import Estado
 
 
-# Create your views here.
 class EstadoListView(ListView):
     model = Estado
-    context_object_name = 'estado_list'
+    context_object_name = "estado_list"
+
+
+def index(request):
+    context = {
+        "base_pages": ["crud", "movimentacao", "relatorio"],
+    }
+    return render(request, "voos/index.html", context)
