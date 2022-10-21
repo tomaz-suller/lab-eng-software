57e44ba26d10be702ba318faff9cd3d7a53ff09c
Eric Gomes <ericog@usp.br>
2022-10-21 16:19:18 -0300

    Add CRUD styling with Bootstrap

--- monitoramento_avioes/urls.py
+++ monitoramento_avioes/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("voos.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
