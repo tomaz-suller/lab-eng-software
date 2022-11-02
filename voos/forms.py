d5bb1138a1719e7dfbb1661fc69835b9b4f0a981
Eric Oliveira Gomes <ericog123456@gmail.com>
2022-11-02 20:11:36 -0300

    Add create and read CRUD functions for CompanhiaAerea

---
+++ voos/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.forms import TextInput
+
+from .models import CompanhiaAerea
+ 
+class CompanhiaAereaForm(forms.ModelForm):
+ 
+    class Meta:
+        model = CompanhiaAerea
+        fields = [
+            "nome",
+            "sigla",
+        ]
+
+        widgets = {
+            'nome': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Nome',
+            }),
+            'sigla': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+               'placeholder': 'Sigla',
+            }),
+        }
--- voos/views.py
+++ voos/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
-from .models import Estado
+from .models import Estado, CompanhiaAerea
+from .forms import CompanhiaAereaForm
 
 
 class EstadoListView(ListView):
     model = Estado
     context_object_name = "estado_list"
+
+
+class CompanhiaAereaListView(ListView):
+    model = CompanhiaAerea
+    context_object_name = "companhia_aerea_list"
+
+
+class CompanhiaAereaCreateView(CreateView):
+    model = CompanhiaAerea
+    # fields = ['nome', 'sigla']
+    form_class = CompanhiaAereaForm
+    success_url = '/crud/companhia-aerea'
 
 
 def index(request):
