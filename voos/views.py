00d2045a06efb35e81d8ffe95e165c7ec99c4eb8
tomaz-suller <tomaz.suller@usp.br>
2022-11-03 15:39:31 -0300

    Add permissions to access class based views

--- voos/views.py
+++ voos/views.py
@@ -1,26 +1,30 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 
-from .models import Estado, CompanhiaAerea
 from .forms import CompanhiaAereaForm
+from .models import CompanhiaAerea, Estado
 
 
-class EstadoListView(ListView):
+class EstadoListView(PermissionRequiredMixin, ListView):
     model = Estado
+    permission_required = "voos.view_estado"
     context_object_name = "estado_list"
 
 
-class CompanhiaAereaListView(ListView):
+class CompanhiaAereaListView(PermissionRequiredMixin, ListView):
     model = CompanhiaAerea
+    permission_required = "voos.view_companhiaaerea"
+    permission_denied_message = "Permissão negada >:("
     context_object_name = "companhia_aerea_list"
 
 
-class CompanhiaAereaCreateView(CreateView):
+class CompanhiaAereaCreateView(PermissionRequiredMixin, CreateView):
     model = CompanhiaAerea
-    # fields = ['nome', 'sigla']
+    permission_required = "voos.add_companhiaaerea"
     form_class = CompanhiaAereaForm
-    success_url = '/crud/companhia-aerea'
+    success_url = "/crud/companhia-aerea"
 
 
 def index(request):
@@ -29,15 +33,18 @@ def index(request):
     }
     return render(request, "voos/index.html", context)
 
+
 def inicio(request):
     return render(request, "voos/inicio.html")
+
 
 def crud(request):
     return render(request, "voos/crud.html")
 
+
 def movimentacao(request):
     return render(request, "voos/movimentacao.html")
 
+
 def relatorio(request):
     return render(request, "voos/relatorio.html")
-
